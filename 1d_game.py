2fde6a49effe29cddac1b22da9b22d79e662a517
Steven Mathey <72931382+sethyvenatem@users.noreply.github.com>
2021-08-02 10:23:45 +0200

    improved version with more features
    - The player can press enter to speed up the projectile
    - The game asks the player's name and puts it on the far left of the path.

--- 1d_game.py
+++ 1d_game.py
@@ -4,21 +4,23 @@ from time import sleep
 import keyboard
 #import string
 import random
+import os
 
 path_length = 100
 sleep_time = 0.15
 opening_sleep_time = 0.5
+fast = 0
 string = '_'*path_length
 
 #prepare the projectile
-projectile_length = 7
+projectile_length = 4
 letters = 'abcdefghijklmnopqrstuvwxyz'
 proj = ''.join(random.choice(letters) for i in range(projectile_length))
 proj_length = len(proj)
 p = list(proj)
 
-# all can be set to zero for debugging.
-all = 1
+# show_all_strings can be set to zero for debugging.
+show_all_strings = 0
 
 #These are the variables for the player answer. k is the answer length and ans is the anser
 ans = ''
@@ -81,47 +83,73 @@ def make_bold(string,n):
 
 def print_sequencially(string,all):
     
-    if all == 0:
+    
+    if all == 1:
         print(string)
-    elif all == 1:
+    elif all == 0:
+        os.system('clear')
         stdout.write("\r")
         stdout.write(string)
         stdout.write("\r")
         stdout.flush()
     else:
         print('What do you want to print')
+        
+def take_inputs(events, s, ans, k, sleep_time, fast):
+    for ev in events:
+        if (ev.event_type == 'down') and not(ev.name in keyboard.all_modifiers):
+            if ev.name == 'space':
+                s[k] = ' '
+                ans = ans + ' '
+                k = k+1
+            elif ev.name == 'enter':
+                if fast == 0:
+                    sleep_time = sleep_time/10
+                    fast = 1
+            else:
+                s[k] = ev.name
+                ans = ans + ev.name
+                k = k+1
+    return s, k, ans, sleep_time, fast
 
-keyboard.start_recording()
-events = keyboard.stop_recording()
+intro = 'Please type in your name and press enter.'
+name = input(intro)
+while len(name) > path_length-1:
+    print_sequencially('Sorry, your name is too long. It will break my game. Please type in another name and press enter.',show_all_strings)
+    name = input()
+print_sequencially('Your name is '+name+'. Press enter to continue.',show_all_strings)
+_ = input()
+name = name + ':'
 
+k = len(name)
 #initialise string
 s = list(string)
 s.append('=o')
-l = len(string)
+s[0:len(name)] = list(name)
 string = "".join(s)
 
-print_sequencially(string,all)
+print_sequencially(string,show_all_strings)
 time.sleep(opening_sleep_time)
 
 #Animate canon
 s = list(string)
 s[-1] = 'O'
 string = "".join(s)
-print_sequencially(string,all)
+print_sequencially(string,show_all_strings)
 time.sleep(opening_sleep_time)
 
 s = list(string)
 s[-1] = '0'
 string = "".join(s)
-print_sequencially(string,all)
+print_sequencially(string,show_all_strings)
 time.sleep(opening_sleep_time)
 
 string = make_bold(string,path_length+1)
-print_sequencially(string,all)
+print_sequencially(string,show_all_strings)
 time.sleep(opening_sleep_time)
 
 string = make_bold(string,path_length)
-print_sequencially(string,all)
+print_sequencially(string,show_all_strings)
 time.sleep(opening_sleep_time)
 
 string = remove_all_bold(string)
@@ -130,14 +158,14 @@ s[-2] = '>'
 string = ''.join(s)
 string = make_bold(string,path_length)
 string = make_bold(string,path_length+1)
-print_sequencially(string,all)
+print_sequencially(string,show_all_strings)
 time.sleep(opening_sleep_time)
 
 #The projectile enters here
 s = list(string)
 s[path_length-1] = p[0]
 string = ''.join(s)
-print_sequencially(string,all)
+print_sequencially(string,show_all_strings)
     
 keyboard.start_recording()
 time.sleep(opening_sleep_time)
@@ -148,14 +176,10 @@ for pos in range(path_length-2,path_length-proj_length-1,-1):
     s[pos:path_length] = p[0:path_length-pos]
 
     
-    for ev in events:
-        if ev.event_type == 'down':
-            s[k] = ev.name
-            ans = ans + ev.name
-            k = k+1
+    s, k, ans, sleep_time, fast = take_inputs(events, s, ans, k, sleep_time, fast)
     
     string = ''.join(s)
-    print_sequencially(string,all)
+    print_sequencially(string,show_all_strings)
     
     keyboard.start_recording()
     time.sleep(sleep_time)
@@ -172,15 +196,10 @@ s[-2] = '='
 s[path_length-proj_length-1:path_length-1] = proj
 s[path_length-1] = '_'
     
-for ev in events:
-    if ev.event_type == 'down':
-        s[k] = ev.name
-        ans = ans + ev.name
-        k = k+1
-
+s, k, ans, sleep_time, fast = take_inputs(events, s, ans, k, sleep_time, fast)
         
 string = "".join(s)
-print_sequencially(string,all)
+print_sequencially(string,show_all_strings)
 
 keyboard.start_recording()
 time.sleep(sleep_time)
@@ -193,24 +212,20 @@ for pos in range(path_length-proj_length-2,-1,-1):
     s[pos:pos+proj_length] = proj
     s[pos+proj_length] = '_'
     
-    for ev in events:
-        if ev.event_type == 'down':
-            s[k] = ev.name
-            ans = ans + ev.name
-            k = k+1
+    s, k, ans, sleep_time, fast = take_inputs(events, s, ans, k, sleep_time, fast)
 
     
     string = "".join(s)
 
-    print_sequencially(string,all)
+    print_sequencially(string,show_all_strings)
     
-    if pos+1 == len(ans):
+    if pos+1 == len(ans)+len(name):
         if ans == proj:
-            print_sequencially('Horray, you win!  Press Esc to quit.',all)
+            print_sequencially('Horray, you win!  Press Esc to quit.',show_all_strings)
             keyboard.wait('esc')
             quit()
         else:
-            print_sequencially('Aw, you loose...  Press Esc to quit.',all)
+            print_sequencially('Aw, you loose...  Press Esc to quit.',show_all_strings)
             keyboard.wait('esc')
             quit()
     else:
@@ -218,6 +233,6 @@ for pos in range(path_length-proj_length-2,-1,-1):
         time.sleep(sleep_time)
         events = keyboard.stop_recording()
         
-print_sequencially('You didn\'t enter anything... Don\'t you like my game?  Press Esc to quit.',all)
+print_sequencially('You didn\'t enter anything... Don\'t you like my game?  Press Esc to quit.',show_all_strings)
 keyboard.wait('esc')
 quit()
